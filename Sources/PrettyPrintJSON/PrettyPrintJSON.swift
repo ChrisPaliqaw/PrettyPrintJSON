5d9fcd5622472b74b17e354d2c8bc56cf4534dc8
Chris Clark <chris@qalang.com>
2020-09-12 22:31:29 +0800

    Complete and tested

--- Sources/PrettyPrintJSON/PrettyPrintJSON.swift
+++ Sources/PrettyPrintJSON/PrettyPrintJSON.swift
@@ -1,3 +1,16 @@
+import Foundation
+
 struct PrettyPrintJSON {
-    var text = "Hello, World!"
+    static func with(data: Data) -> String {
+        do {
+            let data = try JSONSerialization.jsonObject(with: data, options: []) as! [String:AnyObject]
+            let prettyJSON = try JSONSerialization.data(withJSONObject: data, options: JSONSerialization.WritingOptions.prettyPrinted )
+            if let prettyString = String(data: prettyJSON, encoding: String.Encoding.utf8) {
+                return prettyString
+            }
+        } catch {
+            print("Error when pretty printing JSON: \(error)")
+        }
+        return ""
+    }
 }
--- Tests/PrettyPrintJSONTests/PrettyPrintJSONTests.swift
+++ Tests/PrettyPrintJSONTests/PrettyPrintJSONTests.swift
@@ -2,14 +2,44 @@ import XCTest
 @testable import PrettyPrintJSON
 
 final class PrettyPrintJSONTests: XCTestCase {
-    func testExample() {
+    var testObject: ErrorMessage!
+    
+    var testJSON: String!
+    
+    override func setUp() {
+        super.setUp()
+        
+        testObject = ErrorMessage(
+            status_code: 30
+        )
+        
+        testJSON =
+            """
+            {
+              "status_code" : 30
+            }
+            """
+    }
+    
+    override func tearDown() {
+        testObject = nil
+        testJSON = nil
+        super.tearDown()
+    }
+    
+    func testEncodableObject_prettyPrintsCorrectly() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(PrettyPrintJSON().text, "Hello, World!")
+        let data = try! JSONEncoder().encode(testObject)
+        XCTAssertEqual(PrettyPrintJSON.with(data: data), testJSON)
     }
 
     static var allTests = [
-        ("testExample", testExample),
+        ("testEncodableObject_prettyPrintsCorrectly", testEncodableObject_prettyPrintsCorrectly),
     ]
+}
+
+struct ErrorMessage: Encodable {
+    var status_code: Int
 }
